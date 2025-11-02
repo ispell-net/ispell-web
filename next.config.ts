d8619416dc668a71d4136632619c024cb3737233
ispell <contact@ispell.net>
2025-11-03 07:26:30 +0800

    update deply script

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /*
  * @Date: 2025-10-23 09:38:39
- * @LastEditTime: 2025-11-01 21:21:00
+ * @LastEditTime: 2025-11-02 23:38:58
  * @Description: next.js 配置文件
  */
 import type { NextConfig } from 'next';
